81870e7a0b955f89d562587a28d8bacb4387693a
Viktor Jakobsson <vjakobsson@rapidimages.se>
2016-03-30 13:51:10 +0200

    removed return

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 var elasticsearch = require('elasticsearch')
 
 function init (opts) {
-  if (!opts.graphql || !opts.graphql.GraphQLInt) return throw new Error('Missing graphql option, needed for internal reference')
-  if (!opts.elastic.index) return throw new Error('Missing elastic search index to fetch mapping from')
-  if (!opts.elastic.type) return throw new Error('Missing elastic search type to fetch mapping from')
-  if (!opts.mapping) return throw new Error('Missing elastic mapping')
+  if (!opts.graphql || !opts.graphql.GraphQLInt) throw new Error('Missing graphql option, needed for internal reference')
+  if (!opts.elastic.index) throw new Error('Missing elastic search index to fetch mapping from')
+  if (!opts.elastic.type) throw new Error('Missing elastic search type to fetch mapping from')
+  if (!opts.mapping) throw new Error('Missing elastic mapping')
 
   if (opts.elastic.host) {
     opts.client = new elasticsearch.Client({
@@ -17,8 +17,7 @@ function init (opts) {
 
   var schemaBuilder = require('./lib/schemaBuilder')
 
-  var indexes = Object.keys(opts.mapping)
-  var mapping = opts.mapping[indexes[0]].mappings[opts.elastic.type]
+  var mapping = opts.mapping.mappings[opts.elastic.type]
   var properties = mapping.properties
   var transform = mapping.transform
 
