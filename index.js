4f8de0fe4201e8ee35d2d6206e30b43e4ef40aa0
Simon Nord <simon@nord.org>
2016-07-25 21:23:54 +0200

    translate terms correctly, removed elasticsearch dependency

--- index.js
+++ index.js
@@ -1,17 +1,10 @@
-var elasticsearch = require('elasticsearch')
+var debug = require('debug')('elasticsearch-graphql')
 
 function init (opts) {
   if (!opts.graphql || !opts.graphql.GraphQLInt) throw new Error('Missing graphql option, needed for internal reference')
   if (!opts.elastic.index) throw new Error('Missing elastic search index to fetch mapping from')
   if (!opts.elastic.type) throw new Error('Missing elastic search type to fetch mapping from')
   if (!opts.mapping) throw new Error('Missing elastic mapping')
-
-  if (opts.elastic.host) {
-    opts.client = new elasticsearch.Client({
-      host: opts.elastic.host || 'localhost:9200',
-      log: opts.elastic.log || 'trace'
-    })
-  }
 
   require('./graphql').set(opts.graphql)
 
